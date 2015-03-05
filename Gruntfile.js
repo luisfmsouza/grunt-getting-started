d1dda044a870cfd1c97611c988f95fd007bd2c76
Luís Felipe Souza <luisfmsouza@gmail.com>
2015-03-05 12:01:34 -0300

    Imagemin and BrowserSync tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,48 @@ module.exports = function (grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    /* Begin - BrowserSync setup */
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : '../css/*.css'
+        },
+        options: {
+          server: {
+            baseDir: "../"
+          },
+          port: 82
+        }
+      }
+    },
+    /* End - BrowserSync setup */
+
+    /* Begin - ImageMin setup */
+    imagemin: {                          // Task
+      static: {                          // Target
+        options: {                       // Target options
+          optimizationLevel: 3,
+          svgoPlugins: [{ removeViewBox: false }],
+          use: [mozjpeg()]
+        },
+        files: {                         // Dictionary of files
+          'dist/img.png': 'src/img.png', // 'destination': 'source'
+          'dist/img.jpg': 'src/img.jpg',
+          'dist/img.gif': 'src/img.gif'
+        }
+      },
+      dynamic: {                         // Another target
+        files: [{
+          expand: true,                  // Enable dynamic expansion
+          cwd: 'src/',                   // Src matches are relative to this path
+          src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+          dest: 'dist/'                  // Destination path prefix
+        }]
+      }
+    }
+    /* End - ImageMin setup */
+
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
@@ -14,8 +56,17 @@ module.exports = function (grunt) {
     }
   });
 
-  // Load the plugin that provides the "uglify" task.
+  // Load the plugins
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+
+
+  // BrowserSync task
+  grunt.registerTask('sync', ['browserSync']);
+
+  // ImageMin task
+  grunt.registerTask('imgmin', ['imagemin']);
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
