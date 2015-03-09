41f39775ed1a25c18a078d25464fb793272a863b
Luís Felipe Souza <luisfmsouza@gmail.com>
2015-03-09 17:08:03 -0300

    Adding css rules

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,46 +21,102 @@ module.exports = function (grunt) {
     /* End - BrowserSync setup */
 
     /* Begin - ImageMin setup */
-    imagemin: {                          // Task
-      static: {                          // Target
-        options: {                       // Target options
-          optimizationLevel: 3,
-          svgoPlugins: [{ removeViewBox: false }],
-          use: [mozjpeg()]
-        },
-        files: {                         // Dictionary of files
-          'dist/img.png': 'src/img.png', // 'destination': 'source'
-          'dist/img.jpg': 'src/img.jpg',
-          'dist/img.gif': 'src/img.gif'
-        }
-      },
-      dynamic: {                         // Another target
+    imagemin: {
+      dynamic: {
         files: [{
-          expand: true,                  // Enable dynamic expansion
-          cwd: 'src/',                   // Src matches are relative to this path
-          src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
-          dest: 'dist/'                  // Destination path prefix
+          expand: true,
+          cwd: '../images/',
+          src: ['**/*.{png,jpg,gif}'],
+          dest: '../images/'
         }]
       }
-    }
+    },
     /* End - ImageMin setup */
 
+    /* Begin - Uglify JS setup */
     uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-      },
+      // options : {
+      //   mangle                      : true,
+      //   compress                    : true,
+      //   beautify                    : false
+      // },
       build: {
-        src: 'src/<%= pkg.name %>.js',
-        dest: 'build/<%= pkg.name %>.min.js'
+        expand: true,
+        cwd: '../src',
+        src: ['js/**/*.js'],
+        dest: '../',
       }
-    }
+    },
+    /* End - Uglify JS setup */
+
+    /* Begin - CSS Lint setup */
+    csslint : {
+      options : {
+        csslintrc : '.csslintrc'
+      },
+      build : {
+        src : ['../css/**/*.css']
+      }
+    },
+    /* End - CSS Lint setup */
+
+    /* Begin - cssmin setup */
+    cssmin : {
+      build : {
+        files : {
+          '../css/<%= pkg.name %>.min.css' : ['../src/css/**/*.css']
+        }
+      }
+    },
+    /* End - cssmin setup */
+
+    /* Begin - jshint setup */
+    // jshint: {
+    //   options: {
+    //     jshintrc : '.jshintrc'
+    //   },
+    //   build: {
+    //     files : {
+    //       src: ['../src/js/**/*.js'],
+    //     }
+    //   }
+    // },
+    /* End - jshint setup */
+
+    /* Begin - jslint setup */
+    // jslint : {
+    //   build : {
+    //     expand : true,
+    //     src: ['../src/js/**/*.js'],
+    //     exclude : [],
+    //     directives : {
+    //       indent  : 2,
+    //       maxlen  : 160,
+    //       maxerr  : 10,
+    //       white   : true,
+    //       browser : true,
+    //       devel   : true,
+    //       node    : true,
+    //       todo    : true,
+    //       nomen   : true,
+    //       sloppy  : true,
+    //       unparam : true,
+    //       predef  : [
+    //         'jQuery'
+    //       ]
+    //     }
+    //   },
+    // },
+    /* End - jslint setup */
   });
 
-  // Load the plugins
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-browser-sync');
-  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  // Load all plugins dependencies
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
+
+  /**
+  * Tasks
+  **/
 
   // BrowserSync task
   grunt.registerTask('sync', ['browserSync']);
@@ -68,7 +124,16 @@ module.exports = function (grunt) {
   // ImageMin task
   grunt.registerTask('imgmin', ['imagemin']);
 
+  // JS minify task
+  grunt.registerTask('js', ['uglify:build']);
+
+  // CSS minify and concat task
+  grunt.registerTask('css', [/*'csslint:build',*/ 'cssmin:build']);
+
+
+
   // Default task(s).
-  grunt.registerTask('default', ['uglify']);
+  grunt.registerTask('default', ['']);
+
 
 };
